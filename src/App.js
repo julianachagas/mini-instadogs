eb5145fcc96aa9770e9ae9a2c1198a62c319edb3
Juliana Chagas <julianavrchagas@gmail.com>
2023-02-20 11:22:17 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { autoLogin } from './store/login';
+import Content from './components/Content';
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(autoLogin());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Content />
     </div>
   );
 }
